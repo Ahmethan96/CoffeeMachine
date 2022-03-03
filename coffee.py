73abd9442ca8b9c41bc2c4432a057468f1c428a6
ibrahim ahmethan <46056890+Ahmethan96@users.noreply.github.com>
2022-03-03 15:57:35 +0300

    Update coffee.py

--- coffee.py
+++ coffee.py
@@ -18,13 +18,21 @@ flag = True
 while flag:
     def espresso():
         if res["water"] >= in_espresso["water"] and res["coffee"] >= in_espresso["coffee"]:
-            water = res["water"] - in_espresso["water"]
-            coffee = res["coffee"] - in_espresso["coffee"]
-            res["water"] = water
-            res["coffee"] = coffee
-            print(res)
-            # print(f"the rest amount of water is {water}")
-            # print(f"the rest amount of coffee is {coffee}")
+            print("please insert coins")
+            quartar = int(input("how many quartars? "))
+            calc = quartar * 0.25
+            if calc >= 2.5:
+                change = calc - 2.5
+                print(f"here is {change} in change")
+                water = res["water"] - in_espresso["water"]
+                coffee = res["coffee"] - in_espresso["coffee"]
+                res["water"] = water
+                res["coffee"] = coffee
+                print(res)
+
+            elif calc < 2.5:
+                print("sorry that's is not enough money")
+
         elif res["water"] < in_espresso["water"]:
             print("there is no enough water")
         elif res["coffee"] < in_espresso["coffee"]:
@@ -33,17 +41,25 @@ while flag:
             print("there is no enough milk")
             # return flag == False
 
-
-
     def latte():
         if res["water"] >= in_latte["water"] and res["coffee"] >= in_latte["coffee"] and res["milk"] >= in_latte["milk"]:
-            water = res["water"] - in_latte["water"]
-            coffee = res["coffee"] - in_latte["coffee"]
-            milk = res["milk"] - in_latte["milk"]
-            res["water"] = water
-            res["coffee"] = coffee
-            res["milk"] = milk
-            print(res)
+            print("please insert coins")
+            quartar = int(input("how many quartars? "))
+            calc = quartar * 0.25
+            if calc >= 2.5:
+                change = calc - 2.5
+                print(f"here is {change} in change")
+                water = res["water"] - in_latte["water"]
+                coffee = res["coffee"] - in_latte["coffee"]
+                milk = res["milk"] - in_latte["milk"]
+                res["water"] = water
+                res["coffee"] = coffee
+                res["milk"] = milk
+                print(res)
+
+            elif calc < 2.5:
+                print("sorry that's is not enough money")
+
         elif res["water"] < in_latte["water"]:
             print("there is no enough water")
         elif res["coffee"] < in_latte["coffee"]:
@@ -54,12 +70,23 @@ while flag:
 
     def cappuccino():
         if res["water"] >= in_cappuccino["water"] and res["coffee"] >= in_cappuccino["coffee"] and res["milk"] >= in_cappuccino["milk"]:
-            water = res["water"] - in_cappuccino["water"]
-            coffee = res["coffee"] - in_cappuccino["coffee"]
-            milk = res["milk"] - in_cappuccino["milk"]
-            res["water"] = water
-            res["coffee"] = coffee
-            res["milk"] = milk
+                print("please insert coins")
+                quartar = int(input("how many quartars? "))
+                calc = quartar * 0.25
+                if calc >= 2.5:
+                    change = calc - 2.5
+                    print(f"here is {change} in change")
+                    water = res["water"] - in_cappuccino["water"]
+                    coffee = res["coffee"] - in_cappuccino["coffee"]
+                    milk = res["milk"] - in_cappuccino["milk"]
+                    res["water"] = water
+                    res["coffee"] = coffee
+                    res["milk"] = milk
+                    print(res)
+
+                elif calc < 2.5:
+                    print("sorry that's is not enough money")
+
         elif res["water"] < in_cappuccino["water"]:
             print("there is no enough water")
         elif res["coffee"] < in_cappuccino["coffee"]:
@@ -68,59 +95,17 @@ while flag:
             print("there is no enough milk")
             # return flag == False
 
-
-
-    # ask = input("What would you like? (espresso/latte/cappuccino): ")
-    # if ask != "report" and ask != "off":
-    #     order()
-    # elif ask == "off":
-    #     flag = False
-    # elif ask == "report":
-    #     print(res)
-
-
-    def order():
-
-        print("please insert coins")
-        quartar = int(input("how many quartars? "))
-        calc = quartar * 0.25
-
-        if ask == "espresso" and calc >= 2.5:
-            if calc >= 2.5:
-                change = calc - 2.5
-                print(f"here is {change} in change")
-                espresso()
-            elif calc < 2.5:
-                print("sorry that's is not enough money")
-
-            # espresso()
-        elif ask == "latte" and calc >= 2.5:
-            if calc >= 2.5:
-                change = calc - 2.5
-                print(f"here is {change}")
-                latte()
-            elif calc < 2.5:
-                print("sorry that is not enough money")
-            # latte()
-        elif ask == "cappuccino":
-            if calc >= 2.5:
-                change = calc - 2.5
-                print(f"here is {change}")
-                cappuccino()
-        elif calc < 2.5:
-            print("sorry that is not enough money")
-            # cappuccino()
-
-
-
     ask = input("What would you like? (espresso/latte/cappuccino): ")
-    if ask != "report" and ask != "off":
-        order()
+    if ask == "espresso":
+        espresso()
+    elif ask == "latte":
+        latte()
+    elif ask == "cappuccino":
+        cappuccino()
     elif ask == "off":
         flag = False
     elif ask == "report":
         print(res)
-
 
 
 
